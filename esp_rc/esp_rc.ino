f6d09652774a01218977e4453272868ce350fb15
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-08-09 19:04:21 +0900

    -

--- esp_rc/esp_rc.ino
+++ esp_rc/esp_rc.ino
@@ -1,14 +1,61 @@
 #include <WiFi.h>
-#include <ArduinoOTA.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>//https://github.com/me-no-dev/ESPAsyncWebServer
 
 #define I1PWM 0
 #define I2PWM 1
 #define I3PWM 2
 #define I4PWM 3
 #include "util.h"
-const IPAddress ip(192,168,4,1);
+
+const IPAddress ip(192,168,1,1);
 const IPAddress subnet(255, 255, 255, 0);
-WiFiServer server(80);
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+int v[2]={0,0};//L,R
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len){
+	switch(type){
+		case WS_EVT_CONNECT:
+			Serial.printf("ws[%u] connect: %s\n", client->id(), client->remoteIP().toString().c_str());
+			client->printf("{\"purpose\":\"init\",\"cid\":%u,\"cip\":\"%s\",\"max\":%u,\"zpad\":%u}", client->id(), client->remoteIP().toString().c_str(),PWM_MAX,WS_ZERO_PAD);
+			client->ping();
+			break;
+		case WS_EVT_DISCONNECT:
+			Serial.printf("ws[%u] disconnect\n", client->id());
+			v[0]=0;v[1]=0;
+			break;
+		case WS_EVT_ERROR:
+			Serial.printf("ws[%u] error(%u): %s\n", client->id(), *((uint16_t*)arg), (char*)data);
+			break;
+		case WS_EVT_PONG:
+			Serial.printf("ws[%u] pong\n", client->id());
+			break;
+		case WS_EVT_DATA:
+			{
+				AwsFrameInfo *info=(AwsFrameInfo*)arg;
+				if(info->final && info->index==0 && info->len==len && info->opcode==WS_TEXT){
+					data[len]=0;
+					String str=(char*)data;
+					Serial.printf("ws[%u] text-msg[%llu]: %s\n", client->id(), info->len, str.c_str());
+					ws.printfAll("{\"purpose\":\"pong\",\"pong\":\"%s\"}",str.c_str());
+					if(str.startsWith("V_CTR")){
+						// "V_CTR[L<int>][R<int>]"
+						// 0padding needed
+						// example: "V_CTR256256"
+						v[0]=str.substring(5,5+WS_ZERO_PAD).toInt()-PWM_MAX;
+						v[1]=str.substring(5+WS_ZERO_PAD,5+WS_ZERO_PAD+WS_ZERO_PAD).toInt()-PWM_MAX;
+						v[0]=v[0]*5/6;
+						v[1]=v[1]*5/6;
+						Serial.printf("%d %d\n",v[0],v[1]);
+						ws.printfAll("{\"purpose\":\"check\",\"vl\":%d,\"vr\":%d}",v[0],v[1]);
+					}
+				}
+			}
+			break;
+	}
+}
 
 void setup(){
 	Serial.begin(115200);
@@ -19,94 +66,43 @@ void setup(){
 	WiFi.softAP(ssid,pass);
 	delay(100);//https://github.com/espressif/arduino-esp32/issues/985
 	WiFi.softAPConfig(ip,ip,subnet);
-	IPAddress myIP=WiFi.softAPIP();
-	Serial.print("SSID: ");Serial.println(ssid);
-	Serial.print("PASS: ");Serial.println(pass);
-	Serial.print("APIP: ");Serial.println(myIP);
+	Serial.printf("SSID: %s\n",ssid);//Serial.print("SSID: ");Serial.println(ssid);
+	Serial.printf("PASS: %s\n",pass);//Serial.print("PASS: ");Serial.println(pass);
+	Serial.printf("APIP: %s\n",WiFi.softAPIP().toString().c_str());//Serial.print("APIP: ");Serial.println(myIP);
+
+	ws.onEvent(onEvent);
+	server.addHandler(&ws);
+	server.on("/",HTTP_GET,[](AsyncWebServerRequest *request){
+		request->send_P(200,"text/html",html);
+	});
+
 	server.begin();
-	Serial.println("Server start!");
-/*PACKAGE BUG? does not work after first OTA
-	// OTA	https://web.is.tokushima-u.ac.jp/wp/blog/2018/04/12/esp32-arduino-softapでotaプログラム書き込み/
-	ArduinoOTA.setPassword("sazanka_");
-	ArduinoOTA
-		.onStart([](){Serial.print("Start updating ");Serial.println(ArduinoOTA.getCommand()==U_FLASH?"sketch":"filesystem");})
-		.onEnd([](){Serial.println("\nEnd");})
-		.onProgress([](unsigned int progress, unsigned int total){Serial.printf("Progress: %u%%\r",(progress/(total/100)));})
-		.onError([](ota_error_t error){
-			if(error==OTA_AUTH_ERROR)Serial.println("Auth Error");
-			else if(error==OTA_BEGIN_ERROR)Serial.println("Begin Error");
-			else if(error==OTA_CONNECT_ERROR)Serial.println("Connect Error");
-			else if(error==OTA_RECEIVE_ERROR)Serial.println("Receive Error");
-			else if(error==OTA_END_ERROR)Serial.println("End Error");
-		});
-	ArduinoOTA.begin();
-*/
+	Serial.println("server started");
+	/*PACKAGE BUG? does not work after first OTA
+		// OTA	https://web.is.tokushima-u.ac.jp/wp/blog/2018/04/12/esp32-arduino-softapでotaプログラム書き込み/
+		ArduinoOTA.setPassword("sazanka_");
+		ArduinoOTA
+			.onStart([](){Serial.print("Start updating ");Serial.println(ArduinoOTA.getCommand()==U_FLASH?"sketch":"filesystem");})
+			.onEnd([](){Serial.println("\nEnd");})
+			.onProgress([](unsigned int progress, unsigned int total){Serial.printf("Progress: %u%%\r",(progress/(total/100)));})
+			.onError([](ota_error_t error){
+				if(error==OTA_AUTH_ERROR)Serial.println("Auth Error");
+				else if(error==OTA_BEGIN_ERROR)Serial.println("Begin Error");
+				else if(error==OTA_CONNECT_ERROR)Serial.println("Connect Error");
+				else if(error==OTA_RECEIVE_ERROR)Serial.println("Receive Error");
+				else if(error==OTA_END_ERROR)Serial.println("End Error");
+			});
+		ArduinoOTA.begin();
+	*/
 	ledcSetup(I1PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I1,I1PWM);
 	ledcSetup(I2PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I2,I2PWM);
 	ledcSetup(I3PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I3,I3PWM);
 	ledcSetup(I4PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I4,I4PWM);
 }
 
-void loop() {
+void loop(){
 	//ArduinoOTA.handle();
-
-	//http://samuiui.com/2019/06/27/esp32-wroom-32-wifi-でタンクを遠隔操作/
-	//http://mukujii.sakura.ne.jp/esp2.html
-	WiFiClient client=server.available();
-	if(client){
-		String currentLine="";
-		Serial.println("Client Connected.");
-		while(client.connected()){
-			if(client.available()){
-				char c=client.read();
-				Serial.write(c);
-				if(c=='\n'){
-					if(currentLine.length()==0){
-						client.println("HTTP/1.1 200 OK");
-						client.println("Content-type:text/html");
-						client.println();
-						client.print(html);
-						client.println();
-						break;
-					}else{
-						currentLine="";
-					}
-				}else if(c!='\r'){
-					currentLine+=c;
-				}
-
-				if(currentLine.endsWith("GET /?f_")){
-					ledcWrite(I1PWM,0);ledcWrite(I2PWM,PWM_MAX*6/8);
-					ledcWrite(I3PWM,0);ledcWrite(I4PWM,PWM_MAX*6/8);
-				}
-				if(currentLine.endsWith("GET /?fl")){
-					ledcWrite(I1PWM,0);ledcWrite(I2PWM,PWM_MAX*4/8);
-					ledcWrite(I3PWM,0);ledcWrite(I4PWM,PWM_MAX*6/8);
-				}
-				if(currentLine.endsWith("GET /?fr")){
-					ledcWrite(I1PWM,0);ledcWrite(I2PWM,PWM_MAX*6/8);
-					ledcWrite(I3PWM,0);ledcWrite(I4PWM,PWM_MAX*4/8);
-				}
-				if(currentLine.endsWith("GET /?b_")){
-					ledcWrite(I1PWM,PWM_MAX*6/8);ledcWrite(I2PWM,0);
-					ledcWrite(I3PWM,PWM_MAX*6/8);ledcWrite(I4PWM,0);
-				}
-				if(currentLine.endsWith("GET /?bl")){
-					ledcWrite(I1PWM,PWM_MAX*4/8);ledcWrite(I2PWM,0);
-					ledcWrite(I3PWM,PWM_MAX*6/8);ledcWrite(I4PWM,0);
-				}
-				if(currentLine.endsWith("GET /?br")){
-					ledcWrite(I1PWM,PWM_MAX*6/8);ledcWrite(I2PWM,0);
-					ledcWrite(I3PWM,PWM_MAX*4/8);ledcWrite(I4PWM,0);
-				}
-
-				if(currentLine.endsWith("GET /?s_")){
-					ledcWrite(I1PWM,0);ledcWrite(I2PWM,0);
-					ledcWrite(I3PWM,0);ledcWrite(I4PWM,0);
-				}
-			}
-		}
-		client.stop();
-		Serial.println("Client Disconnected.");
-	}
+	ws.cleanupClients();
+	if(v[0]>0){ledcWrite(I3PWM,0);ledcWrite(I4PWM,v[0]);}else{ledcWrite(I3PWM,-v[0]);ledcWrite(I4PWM,0);}
+	if(v[1]>0){ledcWrite(I1PWM,0);ledcWrite(I2PWM,v[1]);}else{ledcWrite(I1PWM,-v[1]);ledcWrite(I2PWM,0);}
 }
--- esp_rc/util.h
+++ esp_rc/util.h
@@ -1,38 +1,66 @@
-const char ssid[]="esp_rc_proto";
-const char pass[]="sazanka_";
-const char html[]=
-"<!DOCTYPE html>\
-<html lang='ja'>\
-<head>\
-	<meta charset='UTF-8'>\
-	<title>esp_rc_proto</title>\
-	<meta name='viewport' content='width=device-width,initial-scale=1'>\
-</head>\
-<body>\
-	<style>\
-		:root{background-color:#222;}\
-		form{text-align:center;}\
-		input{font-size:xx-large;margin:16px;}\
-	</style>\
-	<form method='get'>\
-		<input type='submit' name='fl' value='左前'>\
-		<input type='submit' name='f_' value='前'>\
-		<input type='submit' name='fr' value='右前'><br>\
-		<input type='submit' name='s_' value='停止'><br>\
-		<input type='submit' name='bl' value='左後'>\
-		<input type='submit' name='b_' value='後'>\
-		<input type='submit' name='br' value='右後'>\
-	</form>\
-</body>\
-</html>";
-
 #define I1 18
 #define I2 19
 #define I3 17
 #define I4 16
 
-#define PWM_FREQ 1000.0
+#define PWM_FREQ 1045.0
 #define PWM_BIT 8
 #define PWM_MAX 256
 //PWM_MAX=2^PWM_BIT
 //2^16=65536
+#define WS_ZERO_PAD 3
+//shuuld be grater than length of PWM_MAX
+
+
+const char* ssid="esp_rc_proto";
+const char* pass="sazanka_";
+// QR
+// WIFI:S:<SSID>;T:<WEP|WPA|無記入>;P:<パスワード>;H:<true|false|無記入>;
+const char html[] PROGMEM=R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>esp_rc</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
+	<input type="range" id="vl">
+	<input type="range" id="vr">
+	<pre id="log">Connecting…</pre>
+	<script>
+		'use strict';
+		let ws,data={};
+		const ws_init=()=>{
+			console.log('ws_init');
+			ws=new WebSocket(`ws://${window.location.hostname}/ws`);
+			ws.onopen=e=>{
+				console.log('opened');
+			};
+			ws.onclose=e=>{
+				console.log('closed');
+				setTimeout(ws_init,5000);
+			};
+			ws.onmessage=e=>{
+				(async()=>{
+					data={...data,...JSON.parse(e.data)};
+					log.textContent=JSON.stringify(data,null,'\t');
+					if(data.purpose=="init"){
+						vl.max=vr.max=data.max*2;
+						vl.value=vr.value=data.max;
+						vl.onchange=vr.onchange=()=>ws.send(`V_CTR${vl.value.padStart(data.zpad,'0')}${vr.value.padStart(data.zpad,'0')}`);
+					}
+				})().catch(()=>{
+					log.textContent+=`\n${e.data}`;
+				});
+			};
+			ws.onerror=e=>{
+				console.log(e);
+			};
+		};
+
+		window.onload=ws_init;
+	</script>
+</body>
+</html>
+)rawliteral";
