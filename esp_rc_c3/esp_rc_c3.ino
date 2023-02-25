705c303535e51d31d3a6639f291c5d6ec3e93915
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-02-25 23:27:20 +0900

    -

--- esp_rc_c3/esp_rc_c3.ino
+++ esp_rc_c3/esp_rc_c3.ino
@@ -7,33 +7,34 @@ AsyncWebSocket ws("/ws");
 AsyncWebSocketClient *op=NULL;
 int16_t v[2]={0};
 
-void cliflush(AsyncWebSocket *ws){
-  std::string x="";
-  for(uint8_t i=0;i<ws->count();i++)x=x+std::to_string((*(ws->getClients().nth(i)))->id())+",";
-  if(!x.empty())x.pop_back();
-  ws->printfAll("[\"clis\",%s]\n",x.c_str());
-  ws->printfAll("[\"op\",%u]\n",op->id());
-  Serial.printf("[\"clis\",%s]\n[\"op\",%u]\n",x.c_str(),op->id());
+void flush(AsyncWebSocket *ws){
+  uint8_t l=ws->count()+2,a[l]={1,(uint8_t)op->id()};
+  for(uint8_t i=2;i<l;i++)a[i]=(*(ws->getClients().nth(i-2)))->id();
+  ws->binaryAll(a,l);
+  Serial.printf("clis: ");
+  for(uint8_t i=2;i<l;i++)Serial.printf("%u ",a[i]);
+  Serial.printf(", op: %u\n",a[1]);
 }
 
 void onWS(AsyncWebSocket *ws,AsyncWebSocketClient *client,AwsEventType type,void *arg,uint8_t *data,size_t len){
 	switch(type){
-		case WS_EVT_CONNECT:
-			Serial.printf("ws[%u] conn: %s\n",client->id(),client->remoteIP().toString().c_str());
-			client->printf("[\"init\",{\"id\":%u,\"bit\":%u}]\n",client->id(),PWM_BIT);
+		case WS_EVT_CONNECT:{
+			Serial.printf("conn: %u(%s)\n",client->id(),client->remoteIP().toString().c_str());
+      uint8_t l=3,a[l]={0,(uint8_t)client->id(),PWM_BIT};
+			client->binary(a,l);
       if(op==NULL)op=client;
-      cliflush(ws);
-			break;
+      flush(ws);
+    }break;
 		case WS_EVT_DISCONNECT:
-			Serial.printf("ws[%u] disconn\n",client->id());
+			Serial.printf("disconn: %u\n",client->id());
       if(ws->count()>0){
         if(op==client)op=*(ws->getClients().nth(ws->count()-1));
-        cliflush(ws);
+        flush(ws);
       }else op=NULL;
 			v[0]=0;v[1]=0;
 			break;
-		case WS_EVT_PONG:Serial.printf("ws[%u] pong\n",client->id());break;
-		case WS_EVT_ERROR:Serial.printf("ws[%u] error(%u): %s\n",client->id(),*((uint16_t*)arg),(char*)data);break;
+		case WS_EVT_PONG:Serial.printf("pong: %u\n",client->id());break;
+		case WS_EVT_ERROR:Serial.printf("error: %u(%s)\n",client->id(),*((uint16_t*)arg),(char*)data);break;
 		case WS_EVT_DATA:{
       AwsFrameInfo *info=(AwsFrameInfo*)arg;
       if(info->final&&info->index==0&&info->len==len&&op==client){
@@ -42,15 +43,16 @@ void onWS(AsyncWebSocket *ws,AsyncWebSocketClient *client,AwsEventType type,void
           AsyncWebSocketClient *tmp;
           for(uint8_t i=0;i<ws->count();i++){
             tmp=*(ws->getClients().nth(i));
-            if(tmp->id()==data[1]){op=tmp;cliflush(ws);break;}
+            if(tmp->id()==data[1]){op=tmp;flush(ws);break;}
           }
         }else{
           // velocity [0,L,L,R,R]
           // BE
           v[0]=((data[1]<<8)|data[2])-PWM_MAX;
           v[1]=((data[3]<<8)|data[4])-PWM_MAX;
-          ws->printfAll("[\"vel\",[%d,%d]]",v[0],v[1]);
-          Serial.printf("[\"vel\",[%d,%d]]\n",v[0],v[1]);
+          data[0]=2;
+          ws->binaryAll(data,5);
+          Serial.printf("L: %d, R: %d\n",v[0],v[1]);
         }
       }
     }break;
@@ -59,10 +61,10 @@ void onWS(AsyncWebSocket *ws,AsyncWebSocketClient *client,AwsEventType type,void
 
 
 void setup() {
-  ledcSetup(I1PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I1,I1PWM);
-	ledcSetup(I2PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I2,I2PWM);
-	ledcSetup(I3PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I3,I3PWM);
-	ledcSetup(I4PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I4,I4PWM);
+  ledcSetup(I1PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I1PIN,I1PWM);
+	ledcSetup(I2PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I2PIN,I2PWM);
+	ledcSetup(I3PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I3PIN,I3PWM);
+	ledcSetup(I4PWM,PWM_FREQ,PWM_BIT);ledcAttachPin(I4PIN,I4PWM);
   ledcSetup(RPWM,PWM_FREQ,PWM_BIT);ledcAttachPin(RPIN,RPWM);
   ledcSetup(GPWM,PWM_FREQ,PWM_BIT);ledcAttachPin(GPIN,GPWM);
   ledcWrite(RPWM,PWM_MAX>>2);
@@ -83,7 +85,13 @@ void setup() {
 	svr.begin();
 	Serial.printf("svr started\n");
 
-	ArduinoOTA.setPassword("sazanka_").begin();
+	ArduinoOTA
+    .setHostname(SSID).setPassword(PASS)
+		.onStart([](){Serial.printf("update started: %s\n",ArduinoOTA.getCommand()==U_FLASH?"flash":"spiffs");ws.enable(false);ws.printfAll("update started: %s\n",ArduinoOTA.getCommand()==U_FLASH?"flash":"spiffs");ws.closeAll();})
+		.onEnd([](){Serial.printf("End\n");})
+		.onProgress([](unsigned int x,unsigned int a){ledcWrite(RPWM,PWM_MAX*(a-x)/a>>2);ledcWrite(GPWM,PWM_MAX*x/a>>2);})
+		.onError([](ota_error_t e){Serial.printf("update error: %u\n",e);})
+		.begin();
   Serial.printf("OTA started\n");
 }
 
@@ -91,8 +99,8 @@ void loop() {
   ArduinoOTA.handle();
 	ws.cleanupClients();
 
-  ledcWrite(I1PWM,max(0,+v[0]));ledcWrite(I2PWM,max(0,-v[0]));
-  ledcWrite(I3PWM,max(0,+v[1]));ledcWrite(I4PWM,max(0,-v[1]));
+  ledcWrite(I1PWM,PWM_MAX-max(0,+v[0]));ledcWrite(I2PWM,PWM_MAX-max(0,-v[0]));
+  ledcWrite(I3PWM,PWM_MAX-max(0,+v[1]));ledcWrite(I4PWM,PWM_MAX-max(0,-v[1]));
   #if 1
     if(op){
       ledcWrite(RPWM,(v[0]+PWM_MAX)>>3);
